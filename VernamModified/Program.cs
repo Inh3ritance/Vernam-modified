feeb21a23617dbc1080a1f10d9ccfa9b85a01cf5
Inh3ritance <herreralaces@gmail.com>
2020-12-22 01:15:26 -0800

    Initualizing project components

---
+++ VernamModified/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace VernamModified {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Vernam modified: Enter text to be encrypted.");
+            string originalText = Console.ReadLine();
+            Console.WriteLine("Your input: " + originalText);
+            byte[] data = sha512(originalText);
+            Console.WriteLine(data);
+            Console.WriteLine(data.Length);
+        }
+
+        static byte[] sha512(String str) {
+            byte[] hash;
+            var data = Encoding.UTF8.GetBytes(str);
+            using (SHA512 shaM = new SHA512Managed()) {
+                hash = shaM.ComputeHash(data);
+            }
+            return hash;
+        }
+    }
+}
